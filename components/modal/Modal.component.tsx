62c8876d3322f71825eefca1608e52abf14a35e7
Florian <mesagouiffes@gmail.com>
2021-02-08 16:45:14 +0100

    add closeModal action

--- components/modal/Modal.component.tsx
+++ components/modal/Modal.component.tsx
@@ -1,10 +1,15 @@
 import { ModalPropsTypes } from './modal.types';
 
-const Modal: React.FC<ModalPropsTypes> = ({isOpen}) => {
-  if(!isOpen) return;
+const Modal: React.FC<ModalPropsTypes> = ({children, isOpen, onClose}) => {
+  if(!isOpen) return <></>;
   return (      
     <div>
+      <p onClick={ onClose}>X</p>
       <p>Hello Modal</p>
+      
+      <div>
+        {children}
+      </div> 
     </div>    
   )
 };
--- components/modal/modal.types.ts
+++ components/modal/modal.types.ts
@@ -1,3 +1,4 @@
 export type ModalPropsTypes = {
   isOpen: boolean;
+  onClose: () => void
 }
